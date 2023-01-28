0d9dbd2cbd02b5ae171060f98024d3fae0d04498
Daniel Cano <anuvis96@gmail.com>
2023-01-27 21:47:56 -0500

    Upload files

---
+++ db.php
@@ -0,0 +1,21 @@
+
+<?php
+session_start();
+$conn = mysqli_connect(
+
+    'localhost',
+    'root',
+    '',
+    'php_mysql_crud'
+
+
+);
+
+   /* if(isset($conn)){
+
+    echo 'Db in connected';
+
+    }
+    */
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+
+
+</nav>
